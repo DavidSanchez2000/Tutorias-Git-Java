0bea9f45336013bd2b5141bb55f7e0a2ae58a281
David Sanchez <davidalejandrosanchezarias058@gmail.com>
2023-04-29 14:13:15 -0500

    upload examples

---
+++ Operadores/operator.java
@@ -0,0 +1,12 @@
+class HelloWorld{
+	public static void main(String args[]){
+    int x = 2;
+    int y = 2;
+    int z = 2;
+    int e = 2;
+    e *= -2*(y+z)/3;
+    x = x * (-2*(y+z)/3);
+    System.out.println(e);
+    System.out.println(-2*(y+z)/3);
+    }
+}
